efb5275a1a15e1e3f4dfa5595db9589d41fc982b
Kate Sarfert <sarfertk17@mail.wlu.edu>
2017-03-09 13:14:31 -0500

    Spread

--- server.R
+++ server.R
@@ -15,3 +15,14 @@ shinyServer(function(input, output) {
   })
   
 })
+
+library(tidyverse)
+World_Development_Indicators_3$GDP_in_USd <- 0
+tidyWorldDvelopSpreaded <- spread(World_Development_Indicators_3, key = "GDP_in_USd", value = "GDP (current US$)", fill = NA, convert = FALSE, drop = FALSE, sep = NULL)
+tidyWorldDevelop <- gather(World_Development_Indicators_3, key = "year", value = "GDP (current US$)", 5:61)
+
+
+tidyWorldDevelop2 <- gather(tidyWorldDevelop, key = "year", value = "Adult literacy rate, population 15+ years, both sexes (%)", 5:61)
+tidyWorldDevelop3 <- gather(tidyWorldDevelop2, key = "year", value = "Lifetime risk of maternal death (%)", 5:61)
+tidyWorldDevelop4 <- gather(tidyWorldDevelop3, key = "year", value = "Adult literacy rate, population 15+ years, both sexes (%)", 5:61)
+tidyWorldDevelop5 <- gather(tidyWorldDevelop4, key = "year", value = "Birth rate, crude (per 1,000 people)", 5:61)
