fa93cec37a430c58d6dd98d386481a2794410900
robertsj17 <robertsj17@mail.wlu.edu>
2017-03-21 11:55:28 -0400

    remove practice map

--- server.R
+++ server.R
@@ -52,36 +52,12 @@ world_data <- inner_join(Final_World_Develp_Indicators, Contraceptive, by = c("y
 write.table(world_data, file = "world_data.txt")
 read.table("world_data.txt")
 
-#Map
+#Leaflet Map
 
 library(shiny)
 library(leaflet)
 
-r_colors <- rgb(t(col2rgb(colors()) / 255))
-names(r_colors) <- colors()
 
-ui <- fluidPage(
-  leafletOutput("mymap"),
-  p(),
-  actionButton("recalc", "New points")
-)
-
-server <- function(input, output, session) {
-  
-  points <- eventReactive(input$recalc, {
-    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
-  }, ignoreNULL = FALSE)
-  
-  output$mymap <- renderLeaflet({
-    leaflet() %>%
-      addProviderTiles(providers$Stamen.TonerLite,
-                       options = providerTileOptions(noWrap = TRUE)
-      ) %>%
-      addMarkers(data = points())
-  })
-}
-
-shinyApp(ui, server)
 
 
 
