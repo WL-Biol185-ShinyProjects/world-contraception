614c45569d965ef1d377a8e71b23c059ebb0df2d
ksarfert <sarfertk17@mail.wlu.edu>
2017-03-28 22:04:13 -0400

    Interactive map. Capable of highlighting a country, viewing the country name, and the proportion of married couples that use any type of contraception

--- server.R
+++ server.R
@@ -290,6 +290,37 @@ leaflet(data = geoJSON_map) %>%
               opacity = 1,
               color = "white",
               dashArray = "3",
-              fillOpacity = 0.7)
+              fillOpacity = 0.7,
+              highlight = highlightOptions(
+                weight = 5,
+                color = "#666",
+                dashArray = "",
+                fillOpacity = 0.7,
+                bringToFront = TRUE))
 
+# unfinished labels section
 
+labels <- sprintf(
+  "<strong>%s</strong><br/>%g proportion of married couples who use any contraception",
+  geoJSON_map$name, geoJSON_map$all_contraception_methods
+) %>% lapply(htmltools::HTML)
+
+m_labels <- m %>% addPolygons(
+  fillColor = ~pal(all_contraception_methods),
+  weight = 2,
+  opacity = 1,
+  color = "white",
+  dashArray = "3",
+  fillOpacity = 0.7,
+  highlight = highlightOptions(
+    weight = 5,
+    color = "#666",
+    dashArray = "",
+    fillOpacity = 0.7,
+    bringToFront = TRUE),
+  label = labels,
+  labelOptions = labelOptions(
+    style = list("font-weight" = "normal", padding = "3px 8px"),
+    textsize = "15px",
+    direction = "auto"))
+m_labels
