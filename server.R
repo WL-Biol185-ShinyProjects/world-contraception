5bbb43ef9f7fd9bac69216cf9c00193555a7b916
ksarfert <sarfertk17@mail.wlu.edu>
2017-03-29 20:13:05 -0400

    Making a basic map without interactive qualities first...trying to pass our geoJSON file into the renderLeaflet

--- server.R
+++ server.R
@@ -4,8 +4,11 @@ function(input, output) {
     
   leaflet(data = geoJSON_map) %>%
     addTiles()        %>%
-    
+    addGeoJSON(geoJSON_map) %>%
+      
     if (input$variable1 == "Proportion of married women using any method of contraception") {
+      bins_all_methods <- seq(0, 1, length = 10)
+      pal  <- colorBin("YlOrRd", map@data$all_contraception_methods, bins_all_methods)
     addPolygons(fillColor = ~pal(all_contraception_methods),
                 weight = 2,
                 opacity = 1,
@@ -22,81 +25,11 @@ function(input, output) {
                   labelOptions = labelOptions(
                     style = list("font-weight" = "normal", padding = "3px 8px"),
                     textsize = "10px",
-                    direction = "auto")))
+                    direction = "auto")
+                  ))
 }
-  if (input$"Proportion of married women using modern methods of contraception") {
-      addPolygons(fillColor = ~pal(modern_methods),
-                  weight = 2,
-                  opacity = 1,
-                  color = "white",
-                  dashArray = "3",
-                  fillOpacity = 0.7,
-                  highlight = highlightOptions(
-                    weight = 5,
-                    color = "#666",
-                    dashArray = "",
-                    fillOpacity = 0.7,
-                    bringToFront = TRUE))
-  }
+  
   output$shinymap <- renderLeaflet(shinymap)
- ) }
-
-
-
-
-library(shiny)
-library(tidyverse)
-library(dplyr)
-
-saveRDS(map, file = "geoJSON_map.rds")
-geoJSON_map <- readRDS("geoJSON_map.rds")
-View(geoJSON_map)
-
-# Interactive Map
-library(leaflet)
-
-bins_all_methods <- seq(0, 1, length = 10)
-pal  <- colorBin("YlOrRd", map@data$all_contraception_methods, bins_all_methods)
-
-sinymap <- leaflet(data = geoJSON_map) %>%
-  addTiles()        %>%
-  addPolygons(fillColor = ~pal(all_contraception_methods),
-              weight = 2,
-              opacity = 1,
-              color = "white",
-              dashArray = "3",
-              fillOpacity = 0.7,
-              highlight = highlightOptions(
-                weight = 5,
-                color = "#666",
-                dashArray = "",
-                fillOpacity = 0.7,
-                bringToFront = TRUE))
-
-# unfinished labels section
-
-labels <- sprintf(
-  "<strong>%s</strong><br/>%g proportion of married couples who use any contraception",
-  geoJSON_map$name, geoJSON_map$all_contraception_methods
-) %>% lapply(htmltools::HTML)
-
-m_labels <- m %>% addPolygons(
-  fillColor = ~pal(all_contraception_methods),
-  weight = 2,
-  opacity = 1,
-  color = "white",
-  dashArray = "3",
-  fillOpacity = 0.7,
-  highlight = highlightOptions(
-    weight = 5,
-    color = "#666",
-    dashArray = "",
-    fillOpacity = 0.7,
-    bringToFront = TRUE),
-  label = labels,
-  labelOptions = labelOptions(
-    style = list("font-weight" = "normal", padding = "3px 8px"),
-    textsize = "10px",
-    direction = "auto"))
-m_labels
-
+  }
+)
+}
--- ui.R
+++ ui.R
@@ -3,13 +3,30 @@ library(leaflet)
 fluidPage(
   titlePanel("World Contraception Use and Indicators of Development"),
   sidebarLayout(position = "left",
+                mainPanel = leafletOutput("shinymap"),
                 sidebarPanel(
                   selectInput(inputId = "variable1",
                       label = "Contraception and Development Indicators",
-                      choices = c("Proportion of married women using any method of contraception", "Proportion of married women using modern methods of contraception", "Birth rate", "Death rate", "Infant mortality rate", "Maternal deaths", "Adolescent fertility rate", "Fertility rate", "Male life expectancy", "Female life expectancy", "Male literacy rate", "Female literacy rate", "GDP", "Work force ratio, women to men", "Proportion of men in the labor force", "Proportion of women in the labor force", "Proportion of men who participate in the economy", "Proportion of women who participate in the economy"),
-                      selected = "Proportion of married women using any method of contraception")
-                )
-                ),
-  mainPanel(leafletOutput('shinymap'),
-          plotOutput(outputId = "shinymap")))
+                      choices = list(all_contraception_methods = "Proportion of married women using any method of contraception",
+                                     modern_contraception_methods = "Proportion of married women using modern methods of contraception",
+                                     birth_rate = "Birth rate",
+                                     death_rate = "Death rate",
+                                     infant_mortality_rate = "Infant mortality rate",
+                                     maternal_deaths = "Maternal deaths",
+                                     adolescent_fertility_rate = "Adolescent fertility rate",
+                                     fertility_rate = "Fertility rate",
+                                     male_life_expectancy = "Male life expectancy",
+                                     female_life_expectancy = "Female life expectancy",
+                                     male_lit_rate = "Male literacy rate",
+                                     female_lit_rate = "Female literacy rate",
+                                     gdp = "GDP",
+                                     work_force_ratio = "Work force ratio, women to men",
+                                     male_labor_force_participation = "Proportion of men in the labor force",
+                                     female_labor_force_participation = "Proportion of women in the labor force",
+                                     male_econ_activity = "Proportion of men who participate in the economy",
+                                     female_econ_activity = "Proportion of women who participate in the economy"),
+                      selected = "Proportion of married women using any method of contraception"))
+  )
+              )
+
 
