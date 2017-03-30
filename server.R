785bd9763d1f65fc73858be1cafe36c5767bae65
robertsj17 <robertsj17@mail.wlu.edu>
2017-03-29 23:13:55 -0400

    created popup option where you can click on a country and view the statistics in the panel for the country instead of just having the pass over highlight option. also worked on input selection

--- server.R
+++ server.R
@@ -1,7 +1,11 @@
 bins_all_methods <- seq(0, 1, length = 10)
 pal  <- colorBin("YlOrRd", map@data$all_contraception_methods, bins_all_methods)
 labels <- sprintf(
-  "<strong>%s</strong><br/>%g proportion of married couples who use any contraception",
+  "<strong>%s</strong><br/>%g proportion of married women who use any form of contraception",
+  geoJSON_map$name, geoJSON_map$all_contraception_methods
+) %>% lapply(htmltools::HTML)
+country_popup <-  sprintf(
+  "<strong>%s</strong><br/>%g proportion of married women who use any form of contraception",
   geoJSON_map$name, geoJSON_map$all_contraception_methods
 ) %>% lapply(htmltools::HTML)
 
@@ -13,6 +17,7 @@ function(input, output) {
                   weight = 2,
                   opacity = 1,
                   color = "white",
+                  popup = country_popup,
                   dashArray = "3",
                   fillOpacity = 0.7,
                   highlight = highlightOptions(
