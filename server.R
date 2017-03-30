e81bab51b558e51ba69f33b59d4aa552c0b271de
ksarfert <sarfertk17@mail.wlu.edu>
2017-03-29 21:14:25 -0400

    Published "work in progress" map...includes countries with name labels and data regarding proportion of married women using any contraception. Needs select input

--- server.R
+++ server.R
@@ -1,37 +1,31 @@
+bins_all_methods <- seq(0, 1, length = 10)
+pal  <- colorBin("YlOrRd", map@data$all_contraception_methods, bins_all_methods)
+labels <- sprintf(
+  "<strong>%s</strong><br/>%g proportion of married couples who use any contraception",
+  geoJSON_map$name, geoJSON_map$all_contraception_methods
+) %>% lapply(htmltools::HTML)
+
 function(input, output) {
-  
   output$shinymap <- renderLeaflet({
-    
-  leaflet(data = geoJSON_map) %>%
-    addTiles()        %>%
-    addGeoJSON(geoJSON_map) %>%
-      
-    if (input$variable1 == "Proportion of married women using any method of contraception") {
-      bins_all_methods <- seq(0, 1, length = 10)
-      pal  <- colorBin("YlOrRd", map@data$all_contraception_methods, bins_all_methods)
-    addPolygons(fillColor = ~pal(all_contraception_methods),
-                weight = 2,
-                opacity = 1,
-                color = "white",
-                dashArray = "3",
-                fillOpacity = 0.7,
-                highlight = highlightOptions(
-                  weight = 5,
-                  color = "#666",
-                  dashArray = "",
+      leaflet(data = geoJSON_map) %>%
+      addTiles()        %>%
+      addPolygons(fillColor = ~pal(all_contraception_methods),
+                  weight = 2,
+                  opacity = 1,
+                  color = "white",
+                  dashArray = "3",
                   fillOpacity = 0.7,
-                  bringToFront = TRUE,
+                  highlight = highlightOptions(
+                    weight = 5,
+                    color = "#666",
+                    dashArray = "",
+                    fillOpacity = 0.7,
+                    bringToFront = TRUE),
                   label = labels,
                   labelOptions = labelOptions(
                     style = list("font-weight" = "normal", padding = "3px 8px"),
                     textsize = "10px",
-                    direction = "auto")  %>%
-                    addLegend(pal = pal, values = ~density, opacity = 0.7, title = "Proportion of Married Women Using Birth Control" ,
-                              position = "bottomright")
-                  ))
-}
-  
-  output$shinymap <- renderLeaflet(shinymap)
-  }
-)
+                    direction = "auto"))
+    
+  })
 }
--- ui.R
+++ ui.R
@@ -2,31 +2,13 @@ library(shiny)
 library(leaflet)
 fluidPage(
   titlePanel("World Contraception Use and Indicators of Development"),
-  sidebarLayout(position = "left",
-                mainPanel = leafletOutput("shinymap"),
-                sidebarPanel(
-                  selectInput(inputId = "variable1",
-                      label = "Contraception and Development Indicators",
-                      choices = list(all_contraception_methods = "Proportion of married women using any method of contraception",
-                                     modern_contraception_methods = "Proportion of married women using modern methods of contraception",
-                                     birth_rate = "Birth rate",
-                                     death_rate = "Death rate",
-                                     infant_mortality_rate = "Infant mortality rate",
-                                     maternal_deaths = "Maternal deaths",
-                                     adolescent_fertility_rate = "Adolescent fertility rate",
-                                     fertility_rate = "Fertility rate",
-                                     male_life_expectancy = "Male life expectancy",
-                                     female_life_expectancy = "Female life expectancy",
-                                     male_lit_rate = "Male literacy rate",
-                                     female_lit_rate = "Female literacy rate",
-                                     gdp = "GDP",
-                                     work_force_ratio = "Work force ratio, women to men",
-                                     male_labor_force_participation = "Proportion of men in the labor force",
-                                     female_labor_force_participation = "Proportion of women in the labor force",
-                                     male_econ_activity = "Proportion of men who participate in the economy",
-                                     female_econ_activity = "Proportion of women who participate in the economy"),
-                      selected = "Proportion of married women using any method of contraception"))
+  sidebarLayout(mainPanel = leafletOutput("shinymap"),
+                position = "left",
+                sidebarPanel = ("Variable Selection")
+                )
+               
   )
-              )
+              
+
 
 
