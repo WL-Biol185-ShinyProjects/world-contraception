9ad0123fe451745630665a348d96d441a043bb2e
ksarfert <sarfertk17@mail.wlu.edu>
2017-04-04 14:25:11 -0400

    Changed layout to tabset with Data tab including a sidebarpanel flow for various graphs from the same selectinput

--- ui.R
+++ ui.R
@@ -1,35 +1,43 @@
 library(shiny)
 library(leaflet)
 fluidPage(
-  titlePanel("World Contraception Use and Indicators of Development"),
-  tabsetPanel(
-              tabPanel("Map",
-                       sidebarLayout(mainPanel = leafletOutput("shinymap"),
-                                     position = "left",
-                                     sidebarPanel = sidebarPanel(
-                                       selectInput(inputId = "variable1",
-                                                   label = "Select contraception use or country development indicators to map:",
-                                                   choices = list('Proportion of married women who use any method of contraception' = "all_contraception_methods",
-                                                                  'Proportion of married women using modern methods of contraception' = "modern_contraception_methods",
-                                                                  'Birth rate' = "birth_rate",
-                                                                  'Death rate' = "death_rate",
-                                                                  'Infant mortality rate' = "infant_mortality_rate",
-                                                                  'Maternal deaths' = "maternal_deaths",
-                                                                  'Adolescent fertility rate' = "adolescent_fertility_rate",
-                                                                  'Fertility rate' = "fertility_rate",
-                                                                  'Male life expectancy' = "male_life_expectancy",
-                                                                  'Female life expectancy' = "female_life_expectancy",
-                                                                  'Male literacy rate' = "male_lit_rate",
-                                                                  'Female literacy rate' = "female_lit_rate",
-                                                                  'GDP' = "gdp",
-                                                                  'Work force ratio, women to men' = "work_force_ratio",
-                                                                  'Proportion of men in the labor force' = "male_labor_force_participation",
-                                                                  'Proportion of women in the labor force' = "female_labor_force_participation",
-                                                                  'Proportion of men who participate in the economy' = "male_econ_activity",
-                                                                  'Proportion of women who participate in the economy' = "female_econ_activity"),
-                                                   selected = "Proportion of married women using any method of contraception")))), 
-              tabPanel("Bar Plots"),
-              tabPanel("Correlations"))
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Home", "World Contraception Use and Indicators of Development"),
+      tabPanel("Background"),
+      tabPanel("Data",
+               sidebarLayout( 
+               sidebarPanel(selectInput(inputId = "variable1",
+                           label = "Select contraception use or country development indicators to map:",
+                           choices = list('Proportion of married women who use any method of contraception' = "all_contraception_methods",
+                                          'Proportion of married women using modern methods of contraception' = "modern_contraception_methods",
+                                          'Birth rate' = "birth_rate",
+                                          'Death rate' = "death_rate",
+                                          'Infant mortality rate' = "infant_mortality_rate",
+                                          'Maternal deaths' = "maternal_deaths",
+                                          'Adolescent fertility rate' = "adolescent_fertility_rate",
+                                          'Fertility rate' = "fertility_rate",
+                                          'Male life expectancy' = "male_life_expectancy",
+                                          'Female life expectancy' = "female_life_expectancy",
+                                          'Male literacy rate' = "male_lit_rate",
+                                          'Female literacy rate' = "female_lit_rate",
+                                          'GDP' = "gdp",
+                                          'Work force ratio, women to men' = "work_force_ratio",
+                                          'Proportion of men in the labor force' = "male_labor_force_participation",
+                                          'Proportion of women in the labor force' = "female_labor_force_participation",
+                                          'Proportion of men who participate in the economy' = "male_econ_activity",
+                                          'Proportion of women who participate in the economy' = "female_econ_activity"),
+                           selected = "Proportion of married women using any method of contraception")),
+                mainPanel(
+                          tabsetPanel(
+                                tabPanel("Map", leafletOutput("shinymap"), position = "below"),
+                                tabPanel("Bar Plots"),
+                                tabPanel("Correlations"),
+                                position = "right"))),
+      tabPanel("Sources"))
+    )
   )
+)
+    
   
 
