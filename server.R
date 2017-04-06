eb1cea093795ec4ed921932d9918edf65c5663df
robertsj17 <robertsj17@mail.wlu.edu>
2017-04-06 16:08:10 -0400

    selectize option almost working

--- server.R
+++ server.R
@@ -57,17 +57,17 @@ function(input, output) {
     
   })
   output$barplot <- renderPlot({
-    bp <- geoJSON_map@data %>% 
-      filter(name %in% geoJSON_map$name) %>%
-      ggplot(aes(x = reorder(name, -geoJSON_map@data[[input$variable1]]), y = geoJSON_map@data[[input$variable1]], fill = geoJSON_map@data[[input$variable1]]))
-    
-    if (is.null(input$selectize)){
-      bp <- bp + geom_bar(stat = "identity", alpha = 0.8) + 
-        theme(axis.text.x = element_text(angle = 60, hjust = 1)) + theme(legend.position = "none")
-    }  else {
-      bp <- bp + geom_bar(aes(x = reorder(input$selectize, -geoJSON_map@data[[input$variable1]], y = geoJSON_map@data[[input$variable1]], fill = geoJSON_map@data[[input$variable1]]), stat = "identity", alpha = 0.8)) + 
-        theme(axis.text.x = element_text(angle = 60, hjust = 1)) + theme(legend.position = "none")
+    if(is.null(input$selectize)){
+      x <- geoJSON_map@data$name
+    } else {
+      x <- input$selectize
     }
+    geoJSON_map@data %>% 
+      filter(name %in% geoJSON_map$name) %>%
+      ggplot(aes(x = reorder(name, -geoJSON_map@data[[input$variable1]]), y = geoJSON_map@data[[input$variable1]], fill = geoJSON_map@data[[input$variable1]])) +
+      geom_bar(stat = "identity", alpha = 0.8) + 
+      theme(axis.text.x = element_text(angle = 60, hjust = 1)) + theme(legend.position = "none")
+  
     
   })
   
